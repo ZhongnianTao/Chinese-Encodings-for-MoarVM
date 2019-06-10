a309c2a99d44ea31dda44cb85f2ab283f0ead32f
ZhongnianTao <31367011+ZhongnianTao@users.noreply.github.com>
2019-06-10 21:08:24 +0800

    Week 2

--- gb2312.c
+++ gb2312.c
@@ -17,7 +17,15 @@ MVMString * MVM_string_gb2312_decode(MVMThreadContext *tc, const MVMObject *resu
 		if (0 <= gb2312[i] && gb2312[i] <= 127)
 		{
 			//	ASCII character
-			result->body.storage.blob_32[result_graphs++] = gb2312[i];
+			if (gb2312[i] == '\r' && i+1 < bytes && gb2312[i+1] == '\n')
+			{
+				result->body.storage.blog_32[result_graphs++] = MVM_nfg_crlf_grapheme(tc);
+				i++;
+			}
+			else
+			{
+				result->body.storage.blob_32[result_graphs++] = gb2312[i];
+			}
 		}
 		else
 		{
@@ -30,6 +38,7 @@ MVMString * MVM_string_gb2312_decode(MVMThreadContext *tc, const MVMObject *resu
 				if (index != GB2312_NULL)
 				{
 					result->body.storage.blob_32[result_graphs++] = index;
+					i++;
 				}
 				else
 				{
@@ -46,5 +55,132 @@ MVMString * MVM_string_gb2312_decode(MVMThreadContext *tc, const MVMObject *resu
 	result->body.num_graphs = result_graphs;
 	
 	return result;
+}
+
+MVMuint32 MVM_string_gb2312_decodestream(MVMThreadContext *tc, MVMDecodeStream *ds, 
+					const MVMint32 *stopper_chars, MVMDecodeStreamSeparators *seps)
+{
+	MVMint32 count = 0, total = 0;
+	MVMint32 bufsize;
+	MVMGrapheme32 *buffer = NULL;
+	MVMDecodeStreamBytes *cur_bytes = NULL;
+	MVMDecodeStreamBytes *last_accept_bytes = ds->bytes_head;
+	MVMint32 last_accept_pos, last_was_cr;
+	MVMuint32 reached_stopper;
+	
+	MVMint32 last_was_first_byte;
+	MVMint32 last_codepoint;
+	
+	if (!ds->bytes_head)
+		return 0;
+	last_accept_pos = ds->bytes_head_pos;
+	
+	if (stopper_chars && *stopper_chars == 0)
+		return 1;
+	
+	bufsize = ds->result_size_guess;
+	buffer = MVM_malloc(bufsize * sizeof(MVMGrapheme32));
+	
+	cur_bytes = ds->bytes_head;
+	last_was_cr = 0;
+	reached_stopper = 0;
+	
+	last_was_first_byte = 0;
+	last_codepoint = 0;
+	
+	while (cur_bytes)
+	{
+		MVMint32 pos = cur_bytes == ds->bytes_head ? ds->bytes_head_pos : 0;
+		MVMuint8 *bytes = (MVMuint8 *)cur_bytes->bytes;
+		
+		while (pos < cur_bytes->length)
+		{
+			MVMGrapheme32 graph;
+			MVMuint16 codepoint = bytes[pos++];
+			
+			if (codepoint <= 127)
+			{
+				if (last_was_first_byte)
+				{
+					MVM_exception_throw_adhoc(tc, "Error decoding gb2312 string: invalid gb2312 format (two bytes for a gb2312 character)");
+				}
+				
+	            if (last_was_cr)
+				{
+	                if (codepoint == '\n')
+					{
+	                    graph = MVM_unicode_normalizer_translated_crlf(tc, &(ds->norm));
+	                }
+	                else
+					{
+	                    graph = '\r';
+	                    pos--;
+	                }
+	                last_was_cr = 0;
+	            }
+	            else if (graph == '\r') {
+	                last_was_cr = 1;
+	                continue;
+				}
+				else
+				{
+					graph = codepoint;
+				}
+			}
+			else
+			{
+				if (last_was_first_byte)
+				{
+					MVMuint16 combined_codepoint = last_codepoint * 256 + codepoint;
+					graph = gb2312_cp_to_index(combined_codepoint);
+					if (graph == GB2312_NULL)
+					{
+						MVM_exception_throw_adhoc(tc, "Error decoding gb2312 string: could not decode codepoint 0x%hhX", combined_codepoint);
+					}
+					last_was_first_byte = 0;
+				}
+				else
+				{
+					last_was_first_byte = 1;
+					last_codepoint = codepoint;
+					continue;
+				}
+			}
+			
+			if (count == bufsize)
+			{
+                MVM_string_decodestream_add_chars(tc, ds, buffer, bufsize);
+                buffer = MVM_malloc(bufsize * sizeof(MVMGrapheme32));
+				count = 0;
+			}
+			
+			buffer[count++] = graph;
+			last_accept_bytes = cur_bytes;
+			last_accept_pos = pos;
+			total++;
+			
+			if (MVM_string_decode_stream_maybe_sep(tc, seps, codepoint) ||
+				(stopper_chars && *stopper_chars == total)) {
+				reached_stopper = 1;
+				goto done;
+			}
+		}
+		
+		cur_bytes = cur_bytes -> next;
+	}
+	
+done:
+	
+    if (count)
+	{
+		MVM_string_decodestream_add_chars(tc, ds, buffer, count);
+	}
+    else
+	{
+		MVM_free(buffer);
+	}
+    MVM_string_decodestream_discard_to(tc, ds, last_accept_bytes, last_accept_pos);
+    
+    return reached_stopper;
 }
 
