1c39fc70e9401b33c767adfda848b9d93f5346de
ZhongnianTao <31367011+ZhongnianTao@users.noreply.github.com>
2019-06-23 11:36:10 +0800

    Implemented encode function

--- gb2312.c
+++ gb2312.c
@@ -158,3 +158,82 @@ done:
     return reached_stopper;
 }
 
+char * MVM_string_gb2312_encode_substr(MVMThreadContext *tc, MVMString *str,
+		MVMuint64 *output_size, MVMint64 start, MVMint64 length, MVMString *replacement, 
+		MVMint32 translate_newlines) {
+	
+	MVMuint32 startu = (MVMuint32)start;
+	MVMStringIndex strgraphs = MVM_string_graphs(tc, str);
+	MVMuint32 lengthu = (MVMuint32)(length == -1 ? strgraphs - startu : length);
+	MVMuint8 *result = NULL;
+	size_t result_alloc;
+	MVMuint8 *repl_bytes = NULL;
+	MVMuint64 repl_length;
+	
+	if (start < 0 || start > strgraphs)
+		MVM_exception_throw_adhoc(tc, "start out of range");
+	if (length < -1 || start + lengthu > strgraphs)
+		MVM_exception_throw_adhoc(tc, "length out of range");
+	
+	if (replacement)
+		repl_bytes = (MVMuint8 *) MVM_string_gb2312_encode_substr(tc, 
+			replacement, &repl_length, 0, -1, NULL, translate_newlines);
+	
+	result_alloc = lengthu;
+	result = MVM_malloc(result_alloc + 1);
+	
+	if (str->body.storage_type == MVM_STRING_GRAPHEME_ASCII) {
+		memcpy(result, str->body.storage.blob_ascii, lengthu);
+		result[lengthu]=0;
+		if (output_size)
+			*output_size = lengthu;
+	}
+	else {
+		MVMuint32 out_pos = 0;
+		MVMCodepointIter ci;
+		
+		MVM_string_ci_init(tc, &ci, str, translate_newlines, 0);
+		
+		while (MVM_string_ci_has_more(tc, &ci)) {
+			MVMCodepoint codepoint = MVM_string_ci_get_codepoint(tc, &ci);
+			if (result_alloc <= out_pos + 1) {
+				result_alloc += 8;
+				result = MVM_realloc(result, result_alloc + 2);
+			}
+			if (codepoint <= 0x7F) {
+				result[out_pos++] = codepoint;
+			}
+			else {
+				MVMint32 gb2312_cp;
+                gb2312_cp = gb2312_cp_to_index(codepoint);
+                if (gb2312_cp == GB2312_NULL) {
+                	if (replacement) {
+                		size_t i;
+                		if (result_alloc <= out_pos + repl_length) {
+                			result_alloc += repl_length;
+                			result = MVM_realloc(result, result_alloc + 1);
+                		}
+                		for (i = 0; i < repl_length; i++) {
+                			result[out_pos++] = repl_bytes[i];
+                		}
+                		continue;
+                	}
+                	else {
+                		MVM_free(result);
+                		MVM_exception_throw_adhoc(tc, 
+                			"Error encoding gb2312 string: could not encode codepoint %d",
+                			gb2312_cp);
+                	}
+                }
+                result[out_pos++] = gb2312_cp / 256;
+                result[out_pos++] = gb2312_cp % 256;
+            }
+        }
+        result[out_pos] = 0;
+        if (output_size)
+        	*output_size = out_pos;
+    }
+    MVM_free(repl_bytes);
+    return (char *)result;
+}
+
