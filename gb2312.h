d8e6bdbf08133de19c105556cc9a8c3f8ca75d66
ZhongnianTao <31367011+ZhongnianTao@users.noreply.github.com>
2019-07-28 20:28:51 +0800

    Update source code

--- gb2312.h
+++ gb2312.h
@@ -2,3 +2,4 @@ MVMString * MVM_string_gb2312_decode(MVMThreadContext *tc, const MVMObject *resu
 MVMuint32 MVM_string_gb2312_decodestream(MVMThreadContext *tc, MVMDecodeStream *ds, const MVMint32 *stopper_chars, MVMDecodeStreamSeparators *seps);
 char * MVM_string_gb2312_encode_substr(MVMThreadContext *tc, MVMString *str, MVMuint64 *output_size, MVMint64 start, MVMint64 length, MVMString *replacement, MVMint32 translate_newlines);
 
+
